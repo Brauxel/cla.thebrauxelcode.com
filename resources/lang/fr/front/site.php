4c0128f9ec7855ff338d91997ce259d32ec1d668
Aakash Bhatia <aakash@stocksdigital.com>
2017-07-14 13:30:11 +1000

    init

---
+++ resources/lang/fr/front/site.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Celsius Resources',
+    'sub-title' => 'ASX:CLA',
+    'home' => 'Accueil',
+    'contact' => 'Contact',
+    'blog' => 'Blog',
+    'register' => 'Inscription',
+    'forget-password' => 'Oubli mot de passe',
+    'connection' => 'Connexion',
+    'administration' => 'Administration',
+    'redaction' => 'Rédaction',
+    'logout' => 'Déconnexion',
+    'notification-endtext' => 'Si vous avez des soucis avec le button, copiez le lien ci-après dans votre navigateur :',
+    'salutation' => 'Cordialement',
+    'yes' => 'Oui',
+    'no' => 'Non',
+];
---
+++ resources/lang/pt-BR/front/site.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Celsius Resources',
+    'sub-title' => 'ASX:CLA',
+    'home' => 'Home',
+    'contact' => 'Contato',
+    'blog' => 'Blog',
+    'register' => 'Cadastro',
+    'forget-password' => 'Esqueci minha senha',
+    'connection' => 'Login',
+    'administration' => 'Administração',
+    'redaction' => 'Redação',
+    'logout' => 'Sair',
+    'notification-endtext' => 'Se você está tendo problemas de clicar no botão, copie e cole o URL abaixo no seu navegador web:',
+    'salutation' => 'Saudações',
+    'yes' => 'Sim',
+    'no' => 'Não',
+];
